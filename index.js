b7e5198bbd3cf11402b99107dbc05956610c9088
Jeremy Block <j.block@ufl.edu>
2021-03-12 22:11:17 -0500

    Accessing metadata now - creation date and time to console

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const http = require('http');
+var fs = require("fs");
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+let fileName = '../100MEDIA/DSCF0001.JPG';
+let {birthtime} = fs.statSync(fileName)
+console.log(birthtime.toDateString());
+console.log(birthtime.getHours()+":"+birthtime.getMinutes())
